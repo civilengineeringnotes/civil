3716964c75e4c1d9eba3f189566b8558d7831a89
ROHIT-CHANDEL <62454047+ROHIT-CHANDEL@users.noreply.github.com>
2020-04-10 21:08:26 +0530

    1

--- practical/js/custom.js
+++ practical/js/custom.js
@@ -1,68 +1,69 @@
-/**************************************
-    File Name: custom.js
-    Template Name: Forest Time
-    Created By: HTML.Design
-    http://themeforest.net/user/wpdestek
-**************************************/
-
 (function($) {
     "use strict";
-    $(document).ready(function() {
-        $('#nav-expander').on('click', function(e) {
-            e.preventDefault();
-            $('body').toggleClass('nav-expanded');
-        });
-        $('#nav-close').on('click', function(e) {
-            e.preventDefault();
-            $('body').removeClass('nav-expanded');
-        });
+
+    /* ==============================================
+    ANIMATION -->
+    =============================================== */
+
+    new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    }).init();
+
+    /* ==============================================
+    LIGHTBOX -->
+    =============================================== */
+
+    jQuery('a[data-gal]').each(function() {
+        jQuery(this).attr('rel', jQuery(this).data('gal'));
     });
+    jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
+        animationSpeed: 'slow',
+        theme: 'light_square',
+        slideshow: true,
+        overlay_gallery: true,
+        social_tools: false,
+        deeplinking: false
+    });
+
+    /* ==============================================
+    SCROLL -->
+    =============================================== */
 
     $(function() {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-
-    $('.carousel').carousel({
-        interval: 4000
-    })
-
-    $(window).load(function() {
-        $("#preloader").on(500).fadeOut();
-        $(".preloader").on(600).fadeOut("slow");
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
     });
 
-    jQuery(window).scroll(function(){
-        if (jQuery(this).scrollTop() > 1) {
-            jQuery('.dmtop').css({bottom:"25px"});
-        } else {
-            jQuery('.dmtop').css({bottom:"-100px"});
-        }
+    /* ==============================================
+    VIDEO FIX -->
+    =============================================== */
+
+    $(document).ready(function() {
+        // Target your .container, .wrapper, .post, etc.
+        $(".media").fitVids();
     });
-    jQuery('.dmtop').click(function(){
-        jQuery('html, body').animate({scrollTop: '0px'}, 800);
-        return false;
+
+    /* ==============================================
+    VIDEO FIX -->
+    =============================================== */
+
+    $('.intro>li>a').click(function() {
+        $('.intro>li').removeClass('active');
+        $(this).parent().addClass('active');
     });
 
 })(jQuery);
-
-
-function openCategory(evt, catName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(catName).style.display = "block";
-    evt.currentTarget.className += " active";
-} 
